476437ea130c93e984e2068a5ecb04f208915242
kim young jae <jae2942@gmail.com>
2024-02-02 19:21:29 +0900

    FEAT : navbar adding

--- front/src/layouts/App/index.tsx
+++ front/src/layouts/App/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import loadable from '@loadable/component'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from '@layouts/Navbar';
+import Notification from '@layouts/Notification';
 
 
 const Home = loadable(() => import('@pages/Home'))
 
 function App() {
   return (
-    <div >
-     
+    <div style={{width:'100%',display:'flex',justifyContent:'space-around'}} >
+      <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+        </Routes>
+     <Notification></Notification>
     </div>
   );
 }
