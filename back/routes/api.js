a4032d673b129140522064fa88f292c89d37e0bb
kim young jae <jae2942@gmail.com>
2024-02-05 16:52:04 +0900

    CHORE ; back newapi connect

--- back/routes/api.js
+++ back/routes/api.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const router = express.Router();
+const dotenv = require('dotenv').config()
+const apikey = dotenv.parsed.apiKey
+const NewsAPI = require('newsapi');
+
+const newsapi = new NewsAPI(apikey);
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en'
+}).then(response => {
+  console.log(response);
+})
+
 
 
 module.exports = router;
--- back/server.js
+++ back/server.js
@@ -3,12 +3,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
+
 const apiRouter = require("./routes/api");
 app.set('port', process.env.PORT || 3095); 
 app.use(bodyParser.urlencoded({extended : true})) 
 app.use(express.static(__dirname + "/public")) 
 app.use(express.json()) 
 app.use(cors()) 
+
 
 
 app.use("/api", apiRouter);
